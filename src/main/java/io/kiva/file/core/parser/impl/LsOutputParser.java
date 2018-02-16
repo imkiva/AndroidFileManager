e3b8ad84a00996e0ce510050eb72b7833479c4df
imkiva <libkernelpanic@gmail.com>
2018-02-16 21:48:32 +0800

    Fix: reversed group and owner

--- src/main/java/io/kiva/file/core/parser/impl/LsOutputParser.java
+++ src/main/java/io/kiva/file/core/parser/impl/LsOutputParser.java
@@ -15,8 +15,8 @@ import java.util.regex.Pattern;
  */
 public class LsOutputParser implements IOutputParser {
     private static final int GROUP_PERMISSION = 1;
-    private static final int GROUP_GROUP = 2;
-    private static final int GROUP_OWNER = 3;
+    private static final int GROUP_OWNER = 2;
+    private static final int GROUP_GROUP = 3;
     private static final int GROUP_SIZE = 4;
     private static final int GROUP_TIME = 5;
     private static final int GROUP_NAME = 11;
@@ -24,8 +24,8 @@ public class LsOutputParser implements IOutputParser {
 
     public static final Pattern PATTERN = Pattern.compile(
             "([dlspbc-][rwxtsSX-]{9})\\s*" + // permission
-                    "([a-zA-Z0-9_]+)\\s*" + // group
                     "([a-zA-Z0-9_]+)\\s*" + // owner
+                    "([a-zA-Z0-9_]+)\\s*" + // group
                     "([0-9]*)\\s*?" + // size
                     "(((19|20)[0-9]{2})-(0?[1-9]|1[012])-(0?[1-9]|[12][0-9]|3[01])\\s*([01]?[0-9]|2[0-3]):[0-5][0-9])\\s*" + // time
                     "([^<>/\\\\|:\"*?]+(\\.\\w+)?)\\s*" + // name
